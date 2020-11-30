3c6e0093da48bc9280f30547edd48114097e0b06
imadcollin <imad.collin@gmail.com>
2020-11-30 11:50:41 +0100

    Initial commit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'b28da80ebc5f2c442a42c482021584b4'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'dfffa953-44b7-4f6f-a848-4a9bcde45112'
+};
